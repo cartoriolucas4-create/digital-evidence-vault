c074d2d9819536c3619e7f80ac4c4bdd6cc862c0
cartoriolucas4-create <cartoriolucas4@gmail.com>
2026-09-19 18:38:52 -0300

    Fix TanStack Start client bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
+const router = getRouter();
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
+}
