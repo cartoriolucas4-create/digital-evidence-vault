af0e5cde224d29d109e589691de17b619371d5d2
cartoriolucas4-create <cartoriolucas4@gmail.com>
2026-09-21 14:26:29 -0300

    Habilitar Realtime para sincronização dos dados da conta

---
+++ supabase/migrations/20260921193000_enable_realtime_account_tables.sql
@@ -0,0 +1,26 @@
+DO $$
+DECLARE
+  table_name text;
+BEGIN
+  IF EXISTS (SELECT 1 FROM pg_publication WHERE pubname = 'supabase_realtime') THEN
+    FOREACH table_name IN ARRAY ARRAY[
+      'study_user_state',
+      'study_settings',
+      'study_disciplines',
+      'study_subjects',
+      'study_sources',
+      'study_question_types',
+      'study_entries'
+    ] LOOP
+      IF NOT EXISTS (
+        SELECT 1
+        FROM pg_publication_tables
+        WHERE pubname = 'supabase_realtime'
+          AND schemaname = 'public'
+          AND tablename = table_name
+      ) THEN
+        EXECUTE format('ALTER PUBLICATION supabase_realtime ADD TABLE public.%I', table_name);
+      END IF;
+    END LOOP;
+  END IF;
+END $$;
